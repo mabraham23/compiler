02e26d5c5e66a421c4b84b2468b219998cfd5245
Mark Abraham <markabraham014@gmail.com>
2021-02-10 16:00:46 -0700

    symbols

--- Symbol.cpp
+++ Symbol.cpp
@@ -1,3 +1,66 @@
 #include "Symbol.h"
 
+// returns true if <s> is already in the symbol table.
+bool SymbolTableClass::Exists(const std::string &s){
+    for ( int i=0; i<sybmolTable.size(); i++) {
+        if ( sybmolTable[i].mLabel == s) {
+            return false;
+        }
+    };
+    return true;
+};
+
+// adds <s> to the symbol table, 
+// or quits if it was already there
+void SymbolTableClass::AddEntry(const std::string & s){
+    Variable var;
+    var.mLabel = s;
+    if ( Exists(s) == false ) {
+        sybmolTable.push_back(var);
+    }
+    exit(-1);
+};
+
+// returns the current value of variable <s>, when
+// interpreting. Meaningless for Coding and Executing.
+// Prints a message and quits if variable s does not exist.
+int SymbolTableClass::GetValue(const std::string & s){
+    if ( Exists(s) == false ) {
+        std::cout << "No Variable found for string " << s << std::endl;
+        exit(-1);
+    }
+    int i = GetIndex(s);
+    return sybmolTable[i].mValue;
+    // what to do if there is no value associated with the variable?
+}; 
+
+// sets variable <s> to the given value, when interpreting.
+// Meaningless for Coding and Executing.
+// Prints a message and quits if variable s does not exist.
+void SymbolTableClass::SetValue(const std::string & s, int v){
+    if ( Exists(s) == false ) {
+        std::cout << "No Variable found for string " << s << std::endl;
+        exit(-1);
+    }
+    int i = GetIndex(s);
+    sybmolTable[i].mValue = v;
+}; 
+
+// returns the index of where variable <s> is.
+// returns -1 if variable <s> is not there.
+int SymbolTableClass::GetIndex(const std::string & s){
+    for ( int i=0; i<sybmolTable.size(); i++) {
+        if ( sybmolTable[i].mLabel == s ) {
+            return i;
+        }
+    }
+    return -1;
+}; 
+
+// returns the current number of variables in the symbol
+// table. 
+int SymbolTableClass::GetCount(){
+    return sybmolTable.size();
+};
+
 
--- Symbol.h
+++ Symbol.h
@@ -1,6 +1,8 @@
 #ifndef SYMBOL
 #define SYMBOL
 #include <vector>
+#include <string>
+#include <iostream>
 
 class SymbolTableClass
 {
@@ -8,28 +10,15 @@ private:
     struct Variable {
         std::string mLabel;
         int mValue;
-    }
-    std::vector<Variable> vars;
+    };
+    std::vector<Variable> sybmolTable;
 public:
     bool Exists(const std::string & s); 
-        // returns true if <s> is already in the symbol table.
     void AddEntry(const std::string & s); 
-        // adds <s> to the symbol table, 
-        // or quits if it was already there
     int GetValue(const std::string & s); 
-        // returns the current value of variable <s>, when
-        // interpreting. Meaningless for Coding and Executing.
-        // Prints a message and quits if variable s does not exist.
     void SetValue(const std::string & s, int v); 
-        // sets variable <s> to the given value, when interpreting.
-        // Meaningless for Coding and Executing.
-        // Prints a message and quits if variable s does not exist.
     int GetIndex(const std::string & s); 
-        // returns the index of where variable <s> is.
-        // returns -1 if variable <s> is not there.
     int GetCount(); 
-        // returns the current number of variables in the symbol
-        // table. 
-}
+};
 
 #endif // SYMBOL
